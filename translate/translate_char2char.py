07948d0bea27ecd3e06eef9aeafdd55240013d27
Jim <jrthom18@gmail.com>
2017-02-07 15:57:23 -0700

    Removing FB webhook code

--- translate/translate_char2char.py
+++ translate/translate_char2char.py
@@ -276,88 +276,14 @@ def webhook():
     # endpoint for processing incoming messaging events
 
     data = request.get_json()
-    #log(data)  # you may not want to log every incoming message in production, but it's good for testing
+    log(data)  # you may not want to log every incoming message in production, but it's good for testing
 
-    if data["object"] == "page":
-
-        for entry in data["entry"]:
-            for messaging_event in entry["messaging"]:
-
-                if messaging_event.get("message"):  # someone sent us a message
-
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
-                    post_message_text(message_text)
-                    # Respond by decoding message_text
-                    #handle_typing_bubble(sender_id, True)
-                    message = translator.translate(message_text)
-                    post_bot_response(message)
-                    #handle_typing_bubble(sender_id, False)
-                    #send_message(sender_id, message)
-
-                if messaging_event.get("delivery"):  # delivery confirmation
-                    pass
-
-                if messaging_event.get("optin"):  # optin confirmation
-                    pass
-
-                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
-                    pass
-
+    message_text = data["message_text"]
+    message = translator.translate(message_text)
+    post_bot_response(message)
     return "ok", 200
 
 
-def send_message(recipient_id, message_text):
-
-    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
-
-    params = {
-        "access_token": PAGE_ACCESS_TOKEN
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "text": message_text
-        }
-    })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-    if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)
-
-def handle_typing_bubble(recipient_id, show):
-    params = {
-        "access_token": PAGE_ACCESS_TOKEN
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    if show:
-        data = json.dumps({
-            "recipient": {
-                "id": recipient_id
-            },
-            "sender_action":"typing_on"
-        })
-    else:
-        data = json.dumps({
-            "recipient": {
-                "id": recipient_id
-            },
-            "sender_action":"typing_off"
-        })
-
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-
-def post_message_text(message_text):
-    data = { "message_text": message_text }
-    r = requests.post("http://128.199.126.187/receive_message.php", data=data)
 
 def post_bot_response(message):
     data = { "message_text": message }
